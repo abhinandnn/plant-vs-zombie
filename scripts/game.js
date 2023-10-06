e496fdea5b8465109e8ea04c94f7ded7bb189425
Abhinandan Pandey <abhinandan2210049@akgec.ac.in>
2023-10-06 19:51:47 +0530

    updated background

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,10 @@ const ctx = canvas.getContext('2d');
 const square_size = 100;
 const rows = canvas.height / square_size;
 const columns = canvas.width / square_size;
+const pointer ={
+    x:0,
+    y:0
+};
 
 class Square {
     constructor(x, y, size) {
@@ -30,14 +34,28 @@ for (let i = 1; i < rows; i++) {
         squares[i][j] = square;
     }
 }
+function mouseinsqr(mouseX, mouseY, squareX, squareY, squareSize) {
+    return mouseX >= squareX && mouseX <= squareX + squareSize &&
+        mouseY >= squareY && mouseY <= squareY + squareSize;
+}
 function drawGrid() {
     for (let i = 1; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
+            if(mouseinsqr(pointer.x,pointer.y,squares[i][j].x,squares[i][j].y,square_size))
             squares[i][j].draw();
         }
     }
 }
+canvas.addEventListener('mousemove',function(event) {
+    pointer.x=event.x-canvas.offsetLeft;
+    pointer.y=event.y-canvas.offsetTop;
+})
+canvas.addEventListener('mouseleave',function(event) {
+    pointer.x=0;
+    pointer.y=0;
+})
 function updateGame(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle='grey';
     ctx.fillRect(0,0,ctrl_pnl.w,ctrl_pnl.h);
     drawGrid();
