f7931053843e0bf37151979d884bf598ecf31c24
shreyash <shreyashchaurasia77@gmail.com>
2023-10-07 12:37:19 +0530

    fixing issues in their motion

--- scripts/zombie.js
+++ scripts/zombie.js
@@ -1,5 +1,6 @@
-let MyCanvas = document.getElementById("myCanvas");
- /// collision detection
+ let MyCanvas = document.getElementById("myCanvas");
+
+ // collision detection
  
  const getMouseCoordinates=(event)=>{
     const rect=MyCanvas.getBoundingClientRect();
@@ -7,8 +8,8 @@ let MyCanvas = document.getElementById("myCanvas");
       const y= event.clientY -rect.top;
       return{x,y};
   }
-  
-  canvas.addEventListener("mousemove",(event)=>{
+
+  MyCanvas.addEventListener("mousemove",(event)=>{
     const coordinates=getMouseCoordinates(event);
     const x=coordinates.x;
     const y=coordinates.y;
@@ -16,14 +17,9 @@ let MyCanvas = document.getElementById("myCanvas");
   })
   
   //creating zombie class
-  
- const context=canvas.getContext("2d");
- const zombies_arr=[];
- const zombieTexture=new Image();
- zombieTexture.src="./zombie.png";
- zombieTexture.onload=()=>{
-  class Zombies {
-    constructor(x,y,health){
+
+  class Zombie {
+    constructor(x,y){
      this.x=x;
      this.y=y;
      this.health= 100;
@@ -34,32 +30,94 @@ let MyCanvas = document.getElementById("myCanvas");
     }
      
     draw(context){
-        context.fillstyle='yellow';
-     context.drawRect(this.x,this.y,this.width,this.height);
+        context.fillStyle='yellow';
+     context.fillRect(this.x,this.y,this.width,this.height);
     }
     moveZombie(){
      this.x-=this.speed;
     }
   };
- }
+ const context=MyCanvas.getContext("2d");
+ const zombies_arr=[];
+
+ let MyCanvas = document.getElementById("myCanvas");
+ 
+ /// collision detection
+ 
+ const getMouseCoordinates=(event)=>{
+    const rect=MyCanvas.getBoundingClientRect();
+      const x=event.clientX -rect.left;
+      const y= event.clientY -rect.top;
+      return{x,y};
+  }
+  
+  MyCanvas.addEventListener("mousemove",(event)=>{
+    const coordinates=getMouseCoordinates(event);
+    const x=coordinates.x;
+    const y=coordinates.y;
+    console.log(`mouse coordinates: x=${x},y=${y}`);
+  })
+  
+  //creating zombie class
+
+  class Zombie {
+    constructor(x,y){
+     this.x=x;
+     this.y=y;
+     this.health= 100;
+     this.width=90;
+     this.height=90;
+    
+     this.speed=1.5 ;
+    }
+     
+    draw(context){
+        context.fillStyle='yellow';
+     context.fillRect(this.x,this.y,this.width,this.height);
+    }
+    moveZombie(){
+     this.x-=this.speed;
+    }
+  };
+ const context=MyCanvas.getContext("2d");
+ const zombies_arr=[];
+
  
  const max_zombies=6;
   const createRandomZombie=()=>{
    if(zombies_arr.length<=max_zombies){
-    const maxX = canvas.width-5;
-    const maxY= canvas.height-5;
-    const randomY=Math.random()*maxY;
-     const zombie= new Zombies(maxX,randomY);
+    const maxX = MyCanvas.width;
+    const maxY= MyCanvas.height;
+    const randomY= Math.floor(Math.random()*maxY);
+    
+     const zombie= new Zombie(maxX,randomY);
+
      zombies_arr.push(zombie);
 
    };
   }
 
   const drawZombie=()=>{
-    context.clearRect(0,0,canvas.width,canvas.height);
+    context.clearRect(0,0,MyCanvas.width,MyCanvas.height);
     for(const element of zombies_arr){
       element.moveZombie();
-      element.draw(ctx);
+      element.draw(context);
+    }
+   }
+   // creating a new zombie after 3 seconds
+   setInterval(createRandomZombie, 3000);
+   const gameLoop=()=>{
+    drawZombie();
+    requestAnimationFrame(gameLoop) ;   
+   }
+ gameLoop();
+
+
+  const drawZombie=()=>{
+    context.clearRect(0,0,MyCanvas.width,MyCanvas.height);
+    for(const element of zombies_arr){
+      element.moveZombie();
+      element.draw(context);
     }
    }
    // creating a new zombie after 3 seconds
