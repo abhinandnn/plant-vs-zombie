b2123ebd2518c98716b85c841574250a8b8d737f
Abhinandan Pandey <abhinandan2210049@akgec.ac.in>
2023-10-08 01:15:28 +0530

    added peashooting feature

--- scripts/game.js
+++ scripts/game.js
@@ -8,6 +8,7 @@ const pointer ={
     x:0,
     y:0
 };
+const peas = [];
 
 class Square {
     constructor(x, y, size) {
@@ -16,8 +17,6 @@ class Square {
         this.size = size;
     }
     draw() {
-        // ctx.fillStyle = 'green';
-        // ctx.fillRect(this.x, this.y, this.size, this.size);
         ctx.strokeStyle = 'black';
         ctx.strokeRect(this.x, this.y, this.size, this.size);
     }
@@ -55,28 +54,29 @@ canvas.addEventListener('mouseleave',function(event) {
     pointer.y=0;
 })
 
-// function drawimg(x,l)
+// function drawimg(x,l,dx,dy)
 // {
 // let images=[];
 // images.length=l;
 // for(let i=0;i<l;i++)
 // {
 //     images[i]=new Image();
-//     images[i].src= '/images/' + x + (i+1).toString()+'.png'
+//     images[i].src= '/plants/Chomper/' + x +' (' + (i+1).toString()+').png'
 // }
 // let i=0;
 // setInterval(function(){
-//     ctx.clearRect(0,0,200,200);
+//     ctx.clearRect(100,100,100,100);
 //     if(i>=l-1)
 //     { i=0;}
 //     i++;
 //     ctx.drawImage(images[i],100,100);
-// },100)
-// }
+// },16)}
 let selectedPlantType="peashooter";
-let tsun=150;
+let tsun=300;
 const sunflowerCost=50;
 const peashooterCost=100;
+const chomperCost=100;
+const wallnutCost=50;
 let plantCost=peashooterCost;
 const plants=[];
 class sunflower{
@@ -93,6 +93,17 @@ class sunflower{
 
     }
 } 
+class wallnut{
+    constructor(x,y){
+        this.type="wallnut";
+        this.x=x;
+        this.y=y;
+        this.side=square_size;
+        this.health=400;
+    }
+draw()
+{
+}}
 class peashooter {
     constructor(x,y){
         this.type="peashooter";
@@ -100,14 +111,41 @@ class peashooter {
         this.y=y;
         this.side=square_size;
         this.health=100;
-        this.peas=[];
         this.enemyDetect=false;
+        this.lastPea = 0;
     }
 draw()
 {
-ctx.fillStyle='blue';
+ctx.fillStyle='green';
 ctx.fillRect(this.x,this.y,this.side,this.side)
-}}
+}
+bringPease()
+{
+    const currentTime=Date.now();
+        if(currentTime-this.lastPea>=1000)
+        {
+            peas.push(new pea(this.x+60,this.y+40));
+            this.lastPea=currentTime;
+        }
+    }
+}
+class chomper
+{
+    constructor(x,y)
+    {
+        this.type="chomper";
+        this.x=x;
+        this.y=y;
+        this.side=square_size;
+        this.health=100;
+        this.enemyDetect=false;
+    }
+    draw()
+    {
+
+    }
+}
+
 canvas.addEventListener('click',function()
 {
     const sqrX=pointer.x-(pointer.x%square_size);
@@ -122,28 +160,72 @@ canvas.addEventListener('click',function()
     if (selectedPlantType === "peashooter") {
         selectedPlant = peashooter;
         plantCost = peashooterCost;
-    } else if (selectedPlantType === "sunflower") {
+    }
+    else if (selectedPlantType === "sunflower") {
         selectedPlant = sunflower;
         plantCost = sunflowerCost;
     }
+    else if (selectedPlantType === "chomper") {
+        selectedPlant = chomper;
+        plantCost = chomperCost;
+    }
+    else if (selectedPlantType === "wallnut") {
+        selectedPlant = wallnut;
+        plantCost = wallnutCost;
+    }
+    
     if(tsun>=plantCost)
     {plants.push(new selectedPlant(sqrX,sqrY));
     tsun=tsun-plantCost;}
 })
-function drawPlants(){
+function managePlants(){
     for(let i=0;i<plants.length;i++)
     {
         plants[i].draw();
+        plants[i].bringPease();
     }
+}
+class pea{
+    constructor(x,y)
+    {
+        this.x=x;
+        this.y=y;
+        this.width=20;
+        this.height=20;
+        this.power=20;
+        this.speed=5;
+    }
+    move()
+    {
+        this.x=this.x+this.speed;
+    }
+    draw(){
+        const img=new Image();
+        img.src='/plants/pea1.png';
+        ctx.drawImage(img,this.x,this.y);
+    }
+
+}
+function pease(){
+for(let i=0;i<peas.length;i++)
+{
+peas[i].move();
+peas[i].draw();
+if(peas[i]&&peas[i].x>canvas.width)
+{
+peas.splice(i,1);
+i--;
+}
+console.log('p '+peas.length);
+}
 }
 function updateGame(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle='grey';
     ctx.fillRect(0,0,ctrl_pnl.w,ctrl_pnl.h);
     drawGrid();
-    drawPlants();
+    managePlants();
+    pease();
     setTimeout(updateGame,16);
 }
 updateGame();
-
-
